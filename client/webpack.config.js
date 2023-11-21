68562865a3aaf59b3ba13aa0c3edbd69d209897d
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-11-20 22:35:41 -0500

    updated webpack manifest and set configuration

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,7 +25,26 @@ module.exports = () => {
         title: 'Webpack Plugin',
       }),
       new MiniCssExtractPlugin(),
-      new WorkboxPlugin.GenerateSW(),
+      // new WorkboxPlugin.GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Takes notes with Javascript syntax highlighting',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        orientation: 'portrait',
+        display: 'standalone',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+
+      })
       
     ],
 
