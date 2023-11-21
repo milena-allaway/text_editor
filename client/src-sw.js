4ee7f26a79b9e0731edfd433dd0027e4b570a518
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-11-21 00:10:16 -0500

    added injectmanifest to webpack.config file, deleted generatesw and associated code, since we are using a custom service worker. updated src-sw.js file to include asset-caching and added comments

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,30 +1,54 @@
+// Define workbox modules
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// precache and route assets, ensures that assets used by the 
+//service worker are cached and can be used offline
 precacheAndRoute(self.__WB_MANIFEST);
 
+// set up cache strategy for pages
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
+    // Cache responses with HTTP statuses 0 (error) and 200 (success)
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
+    // Expire page cache after 30 days
     new ExpirationPlugin({
       maxAgeSeconds: 30 * 24 * 60 * 60,
     }),
   ],
 });
 
+// cache the index.html page and the root/home route using the pageCache strategy
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
+// use the pageCache strategy for all navigation requests
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+// register route for caching assets, using the StaleWhileRevalidate strategy,
+// which will use the cached content if available, otherwise fetch from the network
+registerRoute(
+  ({ request }) =>
+    request.destination === 'script' || // cache javascript files
+    request.destination === 'style' || // cache css files
+    request.destination === 'image', // cache image files
+  new StaleWhileRevalidate({
+    cacheName: 'assets-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-const WorkboxPlugin = require('workbox-webpack-plugin');
 // TODO: Add CSS loaders and babel to webpack.
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
@@ -22,10 +21,14 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Webpack Plugin',
+        title: 'JATE',
       }),
       new MiniCssExtractPlugin(),
-      // new WorkboxPlugin.GenerateSW(),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
       new WebpackPwaManifest({
         name: 'Just Another Text Editor',
         short_name: 'JATE',
@@ -65,6 +68,7 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
             presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime'],
             },
           },
         },
