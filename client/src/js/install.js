c4cdc5a78e7d9d80108f519de9727717c1548398
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-11-22 23:56:59 -0500

    updated button views

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,51 +1,49 @@
 const butInstall = document.getElementById('buttonInstall');
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-//set up event handler for beforeinstallprompt event,
-
+//an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    // event.preventDefault();
-    // deferredPrompt = event;
-    // butInstall.style.visibility = 'visible';
-    // butInstall.textContent = 'Install JATE';
-    window.deferredPrompt = event;
-    butInstall.classList.toggle('hidden', false);
+    event.preventDefault();
+    deferredPrompt = event;
+    butInstall.classList.remove('hidden');
+    butInstall.textContent = 'Install Me';
+    // butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-//add event handler for click event on install button,
-//call prompt() on the event to trigger the install prompt
+// Implement a click event handler on the `butInstall` element
 
-butInstall.addEventListener('click', () => {
-    // if (deferredPrompt) {
-    //     deferredPrompt.prompt();
-    //     deferredPrompt.userChoice.then((choiceResult) => {
-    //         if (choiceResult.outcome === 'accepted') {
-    //             console.log('User accepted the install prompt');
-        
-    //         } else {
-    //             console.log('User dismissed the install prompt');
-    //         }
-    //     });
-    //     deferredPrompt = null; //reset the deferredPrompt variable
+butInstall.addEventListener('click', async () => {
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const outcome = await deferredPrompt.userChoice;
+    console.log('User response to the install prompt:', outcome);
+    // prompt has been used, set to null
+    deferredPrompt = null;
 
-    //     butInstall.setAttribute('disabled', true);
-    //     butInstall.textContent = 'Installed';
-    // };
-    const promptEvent = window.deferredPrompt;
-    if (promptEvent) {
-        promptEvent.prompt();
-        window.deferredPrompt = null;
-        butInstall.classList.toggle('hidden', true);
-    }
+    butInstall.classList.add('hidden');
+    // const promptEvent = window.deferredPrompt;
+    // if (promptEvent) {
+    //     promptEvent.prompt();
+    //     window.deferredPrompt = null;
+    //     butInstall.classList.toggle('hidden', true);
+    // }
 });
 
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {
-    console.log('JATE was installed.', event);
+// handler for the `appinstalled` event
+// window.addEventListener('appinstalled', (event) => {
+//     console.log('JATE was installed.', event);
+window.addEventListener('appinstalled', () => {
+    // Clear the deferredPrompt so it can be garbage collected
+    // deferredPrompt = null;
+    console.log('JATE was installed.');
+    // change button text to indicate app was installed
+    deferredPrompt = null;
 });
+
 
 // https://stackoverflow.com/questions/72343203/how-to-add-custom-install-button-for-pwa
 //https://web.dev/articles/customize-install
